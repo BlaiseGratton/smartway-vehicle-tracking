e9047b6a683643a02db50924765ea35a1f5d0c3e
BlaiseGratton <blaisegratton@gmail.com>
2017-12-05 13:35:56 -0600

    add frontend for easy selection of cameras

---
+++ static/app.js
@@ -0,0 +1,36 @@
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyBEueVyUF6ZWIfBu54xiBhWSFQdO_k7Nfg"
+  }
+})
+
+const map = new Vue({
+  el: '#app',
+  data: {
+    message: 'This map',
+    cameras: [],
+    infoWindows: [],
+    zoom: 8,
+    center: {
+      lat: 36.1627,
+      lng: -86.7816
+    },
+
+  },
+  mounted() {
+    axios.get('/api/cameras/')
+      .then(response => this.cameras = response.data,
+            error => console.error(error))
+      .catch(error => console.error(error))
+  },
+  methods: {
+    openInfoWindow: function (camera) {
+      this.infoWindows.push({
+        options: {
+          content: camera.description
+        },
+        position: camera.location.coordinates[0]
+      })
+    }
+  }
+})
